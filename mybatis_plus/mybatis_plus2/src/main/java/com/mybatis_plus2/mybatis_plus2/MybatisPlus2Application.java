5abe845252466d09397d1225dce88a46f26f17a3
mmjnk <67007900+mmjnk@users.noreply.github.com>
2021-03-25 22:21:44 +0800

    Update MybatisPlus2Application.java

--- mybatis_plus/mybatis_plus2/src/main/java/com/mybatis_plus2/mybatis_plus2/MybatisPlus2Application.java
+++ mybatis_plus/mybatis_plus2/src/main/java/com/mybatis_plus2/mybatis_plus2/MybatisPlus2Application.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class MybatisPlus2Application {
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlus2Application.class, args);
+        int a=1;
     }
 
 }
