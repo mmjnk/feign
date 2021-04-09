7cb0000f79e6d1c3ce9ad2c6772b9ef3ba9f1a61
zhouyiming <zhouyiming@tuhu.cn>
2021-04-09 21:03:04 +0800

    color枚举

---
+++ mybatis_plus/mybatis_plus2/src/main/java/com/mybatis_plus2/mybatis_plus2/base/ok/Color.java
@@ -0,0 +1,41 @@
+package com.mybatis_plus2.mybatis_plus2.base.ok;
+
+public enum Color {
+    RED(0,"红色"),
+    BLUE(1,"蓝色"),
+    GREEN(2,"绿色");
+
+    private int code;
+private String desc;
+
+
+    Color(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public  static Color getValue(int code){
+        for (Color color:values()){
+            if (color.getCode()==code){
+                return color;
+            }
+        }
+        return null;
+    }
+}
